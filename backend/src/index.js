b68a74d7cde8dc68ff95ba4dd1a65365af080a88
paulilienescu <paulilienescu@gmail.com>
2026-01-09 14:55:00 +0200

    initial graphql server setup

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at ${url}`);
