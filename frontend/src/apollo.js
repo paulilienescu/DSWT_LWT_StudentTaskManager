12c690b0aa5871e62f1de6ddb21810e879011728
Andreea Ciubotaru <andreeaciubotaru353@yahoo.com>
2026-01-10 21:09:11 +0200

    Auto logout frontend when authentication token is invalid

--- frontend/src/apollo.js
+++ frontend/src/apollo.js
@@ -1,5 +1,11 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  from,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { onError } from "@apollo/client/link/error";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/",
@@ -16,7 +22,21 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  const hasNotAuth =
+    graphQLErrors?.some((e) =>
+      String(e.message || "")
+        .toLowerCase()
+        .includes("not authenticated")
+    ) || false;
+
+  if (hasNotAuth) {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
+});
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache(),
 });
